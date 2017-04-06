c24b725e61bc0eb8b20ab0db3ce5605b0b87d228
RachelBlouin <dono-3@hotmail.com>
2017-04-05 20:58:09 -0400

    Lier les styles RB

--- wp-content/themes/CCIS/index.php
+++ wp-content/themes/CCIS/index.php
@@ -1,33 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title></title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="/assets/css/style.css" rel="stylesheet">
-        <!-- Latest compiled and minified CSS -->
-        <!-- My CSS Style -->
-        <link href="<?php bloginfo("stylesheet_url") ?>" rel="stylesheet">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <title>CCIS</title>
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
- <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
- <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>assets/bootstrap/bootstrap.min.css">
- <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>assets/bootstrap/bootstrap-theme.css">
- <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>assets/bootstrap/main.css">
- <script src="https://use.fontawesome.com/07c6e0fc40.js"></script>
+        <!-- style -->
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/bootstrap/bootstrap-theme.css">
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/style.css">
+        
+        <script src="https://use.fontawesome.com/07c6e0fc40.js"></script>
     <?php wp_head(); ?>
     </head>
+
+
     <body>
     <main>
-    <div class="container">
-        <h2 class="hello">Hello welcome to my world btiches</h2>
-    </div>
+        
     </main>
     <footer>
+    
     </footer>
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/bootstrap.min.js"></script>
+
+
     </body>
 </html>
