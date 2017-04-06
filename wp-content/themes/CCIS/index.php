a6ab0ec8f29394330f2289bab457920b0812fc7f
jim102293 <jimmz102293@hotmail.ca>
2017-04-05 20:31:20 -0400

    Ajout des stylesheet JD

--- wp-content/themes/CCIS/index.php
+++ wp-content/themes/CCIS/index.php
@@ -1,1 +1,33 @@
-<h2>testing rachel 14:25</h2>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/assets/css/style.css" rel="stylesheet">
+        <!-- Latest compiled and minified CSS -->
+        <!-- My CSS Style -->
+        <link href="<?php bloginfo("stylesheet_url") ?>" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+ <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+ <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>assets/bootstrap/bootstrap.min.css">
+ <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>assets/bootstrap/bootstrap-theme.css">
+ <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>assets/bootstrap/main.css">
+ <script src="https://use.fontawesome.com/07c6e0fc40.js"></script>
+    <?php wp_head(); ?>
+    </head>
+    <body>
+    <main>
+    <div class="container">
+        <h2 class="hello">Hello welcome to my world btiches</h2>
+    </div>
+    </main>
+    <footer>
+    </footer>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </body>
+</html>
