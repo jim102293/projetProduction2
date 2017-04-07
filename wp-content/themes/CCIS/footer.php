70f79c6e9aea672a9082e8efa39871273bf3a9e3
RachelBlouin <dono-3@hotmail.com>
2017-04-07 09:51:03 -0400

    footer commencer.

--- wp-content/themes/CCIS/footer.php
+++ wp-content/themes/CCIS/footer.php
@@ -1,5 +1,41 @@
 <footer>
-    
+
+<!-- FOOTER PARTENAIRES -->
+
+
+    <!--********  FOOTER INFO ******** -->
+    <div class="container-fluid">
+        <div class="row">
+            <!-- NOUS JOINDRE -->
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                <div class="bloc-info-footer">
+                    <h2 class="footer-contact-h2">NOUS JOINDRE</h2>
+                    <p class="adresse-footer">1250, avenue de la Station,<br>bureau 1-116  <br>Shawinigan (Québec)<br>G9N 1W8<br>Téléphone : 819.536.0777<br>info@ccishawinigan.ca</p>
+                </div>    
+            </div>
+            <!-- infolettre -->
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                <div class="bloc-infolettre-footer">
+                    INFOLETTRE
+                </div>
+            </div>
+            <!-- Réseaux sociaux-->
+            <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                 <div class="bloc-reseaux-footer">
+                    <h3 class="h3-reseauxsociaux">Suivez-nous</h3>
+                 </div>
+            </div>
+        </div><!-- fin row -->
+
+
+            <!-- barre noire -->
+            <div class="barre-footer">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <h3 class="h3-barre-noir">© 2017 | CHAMBRE DES COMMERCES ET INDUSTRIE SHAWINIGAN</h3>
+                </div>
+            </div>
+        
+    </div><!-- fin container fluid -->
 </footer>
 
    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
