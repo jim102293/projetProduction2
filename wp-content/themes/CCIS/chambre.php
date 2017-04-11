bd1b6b00d67aa7ee4558a52f3ed42918f0fd50d8
RachelBlouin <dono-3@hotmail.com>
2017-04-11 11:41:35 -0400

    envoi test

--- wp-content/themes/CCIS/chambre.php
+++ wp-content/themes/CCIS/chambre.php
@@ -6,13 +6,35 @@ Template Name: Chambre Page
 
 <?php get_header();?>
 
+<!--TITRE DE LA PAGE + FILS D'ARIANE' -->
+    <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-12">
+                        <!-- Fils d'ariane-->
+                        <div class="breadcrumbs">
+                            <ul class="breadcrumb">
+                                <li><a href="<?php bloginfo('url') ?>">Accueil</a></li>
+                                <li><a href="#"><?php the_title(); ?></a></li>
+                            </ul>
+                        </div> <!-- fin div breadcrumbs -->
+
+                        <h2 class="h2-titre-page">
+                             <?php the_title(); ?>
+                        </h2>
+                    </div> <!-- fin div col -->
+                </div> <!-- fin div row -->
+    </div> <!-- fin container -->
+
+ <div class="thumbnail">
  <?php while ( have_posts() ) : the_post(); ?>
-    <div class="thumbnail">
+   
                        <?php if ( has_post_thumbnail() ) {
 	                        the_post_thumbnail();
                                         } 
-    ?>
+                        ?>
 </div> 
+
+
 
  <?php endwhile; // end of the loop. ?>
 <?php get_footer(); ?>
