324ceaad002277db6339f8ef5aefdaf9e00807cb
RachelBlouin <dono-3@hotmail.com>
2017-04-12 10:16:30 -0400

    zone membre + galerie photo +

--- wp-content/themes/CCIS/galleriephoto.php
+++ wp-content/themes/CCIS/galleriephoto.php
@@ -25,7 +25,7 @@ Template Name: Gallerie Photo
 <div class="row">
      <?php while ( have_posts() ) : the_post(); ?>
                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <img class="img-responsive" id="image1" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/headerchambre.png" alt="">
+                    <img class="img-responsive" id="image1" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/galerie-photo.png" alt="">
                 </div>
         </div>
 </div> 
