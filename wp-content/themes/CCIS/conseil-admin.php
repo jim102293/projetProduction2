449fd44e01f495254ade9ae37be18d6f6858eb57
RachelBlouin <dono-3@hotmail.com>
2017-04-11 21:08:51 -0400

    template conseil admin fait

---
+++ wp-content/themes/CCIS/conseil-admin.php
@@ -0,0 +1,50 @@
+<?php
+/*
+Template Name: Conseil admin Page
+*/
+?>
+
+<?php get_header();?>
+
+<!--TITRE DE LA PAGE + FILS D'ARIANE' -->
+    <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <!-- Fils d'ariane-->
+                        <div class="breadcrumbs">
+                            <ul class="breadcrumb">
+                                <li><a href="<?php bloginfo('url') ?>">Accueil</a></li>
+                                <li><a href="#"><?php the_title(); ?></a></li>
+                            </ul>
+                        </div> <!-- fin div breadcrumbs -->
+
+                        <h2 class="h2-titre-page">
+                             <?php the_title(); ?>
+                        </h2>
+                    </div> <!-- fin div col -->
+                </div> <!-- fin div row -->
+    </div> <!-- fin container -->
+
+ <div class="thumbnail">
+ <?php while ( have_posts() ) : the_post(); ?>
+   
+                       <?php if ( has_post_thumbnail() ) {
+	                        the_post_thumbnail();
+                                        } 
+                        ?>
+<div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <img class="img-responsive" id="image1" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/conseil-admin.png" alt="">
+                </div>
+        </div>
+</div> 
+<div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <p><?php the_content()?></p>
+                </div>
+        </div>
+</div> 
+
+
+ <?php endwhile; // end of the loop. ?>
+<?php get_footer(); ?>
