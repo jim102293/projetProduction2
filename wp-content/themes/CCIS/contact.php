bb9384eb8d4c225e9c39e26b095d4a5dd32e1b67
RachelBlouin <dono-3@hotmail.com>
2017-04-12 14:41:20 -0400

    page contact

--- wp-content/themes/CCIS/contact.php
+++ wp-content/themes/CCIS/contact.php
@@ -13,17 +13,29 @@ Template Name: Contact Page
                 </div>
             </div>
         </div>
-    </section>  
+    </section> 
+
+
+<div class="container-fluid">
      <?php while ( have_posts() ) : the_post(); ?>
+
      <div class="row">
+
          
-         <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4 float">
+         <div class="col-xs-12 col-sm-12 col-md-5 col-lg-5">
+              <?php the_content()?>   
+         </div>
+         
+         
+         <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 float">
              <?php echo do_shortcode("[huge_it_maps id='2']"); ?>
          </div>
          
-     </div>          
+          
                  
     </div> <!-- FIN DU ROW -->
+</div>
+
     <?php endwhile; // end of the loop. ?>
     </main>
   
