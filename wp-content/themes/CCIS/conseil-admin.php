93291964c21290067cfbe57e99bd8d6abbde39bb
jim102293 <jimmz102293@hotmail.ca>
2017-04-12 22:34:18 -0400

    conseil

--- wp-content/themes/CCIS/conseil-admin.php
+++ wp-content/themes/CCIS/conseil-admin.php
@@ -25,46 +25,50 @@ Template Name: Conseil admin Page
                 </div> <!-- fin div row -->
     </div> <!-- fin container -->
 
- <div class="thumbnail">
- <?php while ( have_posts() ) : the_post(); ?>
-   
-                       <?php if ( has_post_thumbnail() ) {
-	                        the_post_thumbnail();
-                                        } 
-                        ?>
 <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                     <img class="img-responsive" id="image1" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/conseil-admin.png" alt="">
                 </div>
         </div>
-</div> 
-<div class="row">
+
              
-                                    <div class="col-sm-4 col-lg-4 col-md-4">
-                        <div class="thumbnail">
-                      <?php $loop = new WP_Query( array( 'post_type' => 'membre', 'posts_per_page' => -1 ) ); ?>
-                <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-                    <?php if ( has_post_thumbnail() ) {
+                                    
+                                       
+                       
+                      
+               
+                   
+                                     
+                                         
+                                
+                                <?php $loop = new WP_Query( array( 'post_type' => 'membre', 'posts_per_page' => -1 ) ); ?>
+                                <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                                <div class="container-fluid"> 
+                                 <div class="row">
+                                <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                                <div class="thumbnail">
+                                 
+                                <?php if ( has_post_thumbnail() ) {
                                             the_post_thumbnail();
                                                         } 
                         
                                          ?>
-                                    
-                                         <div class="caption">
+                                
                                 <h4><a href="<?php the_permalink() ?>"><?php the_title();?></a>
                                 </h4>
                                 <h3><?php the_excerpt();?></h3>
                                 <h4><?php the_field('Adresse'); ?></h4>
                                 <h4><?php the_field('Entreprise'); ?></h4>
                                 <h4><?php the_field('Courriel'); ?></h4>
+                                
+                                </div>
                             </div>
-                            <?php endwhile; wp_reset_query(); ?>
-                </div>
-                
-        </div>
-</div> 
+                     
+                           <?php endwhile; wp_reset_query(); ?>
+                           </div>   
+ 
 
-    
 
- <?php endwhile; // end of the loop. ?>
+
+
 <?php get_footer(); ?>
